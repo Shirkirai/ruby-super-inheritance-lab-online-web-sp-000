aacc20c7422301f0df8f08671d02e2324719d5ec
Jung Lee <lee.jung.s95@gmail.com>
2020-06-05 03:24:51 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,5 +3,6 @@ class ChattyStudent < Student
   def hello
     super
     "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died."
+  end
 
 end
